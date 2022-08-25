1edc5023559e85c4c2662b0f2dac657faab8f594
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2022-08-26 02:56:29 +0700

    Update src.py

--- app/src.py
+++ app/src.py
@@ -3,11 +3,11 @@ from flask import render_template
 from devices.devices_service import DevicesService
 
 def base_regestry(app):
-    devices_seervice = DevicesService()
+    devices_service = DevicesService()
 
     @app.route('/')
     def index():
-        devices = devices_seervice.get_all_devices()
+        devices = devices_service.get_all_devices()
         return render_template('index.html')
         #return render_template('index.html', devices = devices)
 
